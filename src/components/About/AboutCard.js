9f7dba21659f02054d843df6adcf0434038bc85b
Yaoyi <wangyaoyi@Yaoyis-MacBook-Pro.local>
2024-02-01 05:45:24 -0800

    Added contents of Personal Portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+          Hi Everyone, I am <span className="purple">Yaoyi Wang </span>
+            from <span className="purple"> Guizhou, China.</span>
+            <br />
+            I enrolled in Computer Science - Align Program at Northeastern University in downtown Vancouver.
+            <br />
+            I am also employed as a Overseas Operations freelance at Voyager Global. 
+            <br />
+            <br />
+            Apart from coding, some other activities that I love:
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Movies & Music
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Galleries & Museums
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Life shrinks or expands in proportion with one’s courage"{" "}
+          </p>
+          <footer className="blockquote-footer">Anaïs Nin</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.JPG";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              Let me <span className="purple"> introduce </span> myself
+            </h1>
+            <p className="home-about-body">
+              Transitioning from a Bachelor of Arts to the dynamic world of technology, 
+              I embarked on an exciting journey at Northeastern University through the Computer Science - Align program.
+              My passion for embracing new technologies has driven me forward,
+              and I am actively seeking co-op/intern opportunities in software development or QA testing to apply my growing knowledge.
+              <br />
+              <br />
+              My academic adventure began with Python and the foundational principles of discrete structures(23FA).
+              This semester(24SP), I'm delving into Java, C, and the intricate world of data structures and algorithms.
+              Beyond the classroom, I've been independently exploring the realms of web development,
+              mastering tools like HTML, CSS, JavaScript, jQuery, Node.js, Express.js, React, and various Web APIs to broaden my skill set.
+              <br />
+              <br />
+              With a heart open to learning and a mind brimming with ideas, 
+              I am ready to dive into the <b className="purple">Tech</b> world.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>Find me on</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/YaoyiW27"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/yaoyiw20010527/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
