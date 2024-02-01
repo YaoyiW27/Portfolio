4abfb502e3cc574a7a5978578e1ddeb0e252c995
Yaoyi (Betty) Wang <129607949+YaoyiW27@users.noreply.github.com>
2024-02-01 14:25:54 -0800

    edited Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/YaoyiW27"
+                href="https://github.com/YaoyiW27/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
