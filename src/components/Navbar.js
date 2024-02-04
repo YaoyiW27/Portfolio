56a4088ff8fc1a72fed423faa1e97a249bb3a7e9
Yaoyi <wangyaoyi@Yaoyis-MacBook-Pro.local>
2024-02-03 21:11:30 -0800

    change blog name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -87,7 +87,7 @@ function NavBar() {
                 target="_blank"
                 rel="noreferrer"
               >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
+                <ImBlog style={{ marginBottom: "2px" }} /> Blog
               </Nav.Link>
             </Nav.Item>
 
